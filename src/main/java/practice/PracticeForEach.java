3f66738e0c722728d5874d1d21f4ad93c91a030e
栗田駿 <shun3533@softbank.ne.jp>
2024-05-22 16:49:14 +0900

    third commit

--- src/main/java/practice/PracticeForEach.java
+++ src/main/java/practice/PracticeForEach.java
@@ -1,8 +1,10 @@
 package practice;
 
 public class PracticeForEach {
-	String name[] = {"田原", "福尾", "倉成","濱野", "前村", "重永","高林", "鈴木", "栗田","轟"};
+	public static void main(String[] args) {
+		String [] name = {"田原", "福尾", "倉成","濱野", "前村", "重永","高林", "鈴木", "栗田","轟"};
 	for (String str: name){
 		  System.out.println(str);
+	}
 	}
 }
--- src/main/java/practice/PracticeSwitch.java
+++ src/main/java/practice/PracticeSwitch.java
@@ -2,27 +2,25 @@ package practice;
 
 public class PracticeSwitch {
 	public static void main(String[] args){
-	    int[] classcode = {1,2,3,4,5};
-	    String[] name = {"勇者","バトルマスター","パラディン","海賊","魔法剣士"};
+	    int[] classcode = {1,2,4,1,5,5,2,1,3,1};
 
-	    for (int i = 0; i < 5; i++){
-	      System.out.print(name[i]);
+	    for (int i = 0; i <= 9; i++){
 
 	      switch(classcode[i]){
 	        case 1:
-	          System.out.println();
+	          System.out.println("1:勇者");
 	          break;
 	        case 2:
-	          System.out.println();
+	          System.out.println("2:バトルマスター");
 	          break;
 	        case 3:
-	          System.out.println();
+	          System.out.println("3:パラディン");
 	          break;
 	        case 4:
-	          System.out.println();
+	          System.out.println("4:海賊");
 	          break;
 	        case 5:
-	          System.out.println();
+	          System.out.println("5:魔法剣士");
 	          break;
 	      }
 	    }
